cdb068757c85f8e397c7496081c3edb862685644
Mathias <Mathias.Halmetschlager@gmx.at>
2017-01-03 11:59:53 +0100

    implemented RMI object-registration and -lookup

--- src/main/java/nameserver/Nameserver.java
+++ src/main/java/nameserver/Nameserver.java
@@ -3,19 +3,31 @@ package nameserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
+import cli.Shell;
+import nameserver.exceptions.AlreadyRegisteredException;
+import nameserver.exceptions.InvalidDomainException;
 import util.Config;
 
 /**
  * Please note that this class is not needed for Lab 1, but will later be used
  * in Lab 2. Hence, you do not have to implement it for the first submission.
  */
-public class Nameserver implements INameserverCli, Runnable {
+public class Nameserver implements INameserverCli, INameserver,INameserverForChatserver, Runnable {
 
 	private String componentName;
 	private Config config;
 	private InputStream userRequestStream;
 	private PrintStream userResponseStream;
+	private Registry registry;
+	private INameserver rootNS;
+	private Shell shell;
 
 	/**
 	 * @param componentName
@@ -33,6 +45,51 @@ public class Nameserver implements INameserverCli, Runnable {
 		this.config = config;
 		this.userRequestStream = userRequestStream;
 		this.userResponseStream = userResponseStream;
+
+		shell = new Shell(componentName, userRequestStream, userResponseStream);
+
+		shell.register(this);
+
+		//If the config does not contain "domain" this Nameserver is the root-ns
+		if(!config.listKeys().contains("domain")){
+			try {
+				// create and export the registry instance on localhost at the
+				// specified port
+				registry = LocateRegistry.createRegistry(config
+						.getInt("registry.port"));
+
+				// create a remote object of this server object
+				INameserver remote = (INameserver) UnicastRemoteObject
+						.exportObject(this, 0);
+				// bind the obtained remote object on specified binding name in the
+				// registry
+				registry.bind(config.getString("root_id"), remote);
+			} catch (RemoteException e) {
+				throw new RuntimeException("Error while starting server.", e);
+			} catch (AlreadyBoundException e) {
+				throw new RuntimeException(
+						"Error while binding remote object to registry.", e);
+			}
+		}else{
+			//Regular Nameserver (not root-ns)
+			try {
+				// obtain registry that was created by the server
+				registry = LocateRegistry.getRegistry(
+						config.getString("registry.host"),
+						config.getInt("registry.port"));
+				// look for the bound server remote-object implementing the IServer
+				// interface
+				rootNS = (INameserver) registry.lookup(config
+						.getString("root_id"));
+
+			} catch (RemoteException e) {
+				throw new RuntimeException(
+						"Error while obtaining registry/server-remote-object.", e);
+			} catch (NotBoundException e) {
+				throw new RuntimeException(
+						"Error while looking for server-remote-object.", e);
+			}
+		}
 
 		// TODO
 	}
@@ -71,4 +128,23 @@ public class Nameserver implements INameserverCli, Runnable {
 		// TODO: start the nameserver
 	}
 
+	@Override
+	public void registerNameserver(final String domain, final INameserver nameserver, final INameserverForChatserver nameserverForChatserver) throws RemoteException, AlreadyRegisteredException, InvalidDomainException {
+
+	}
+
+	@Override
+	public void registerUser(final String username, final String address) throws RemoteException, AlreadyRegisteredException, InvalidDomainException {
+
+	}
+
+	@Override
+	public INameserverForChatserver getNameserver(final String zone) throws RemoteException {
+		return null;
+	}
+
+	@Override
+	public String lookup(final String username) throws RemoteException {
+		return "serverresponse";
+	}
 }
