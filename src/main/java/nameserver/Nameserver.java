d955eb7693019d6244d4f16388c5b7e001e71979
mkamleithner <martin.kamleithner@gmail.com>
2017-01-04 19:54:35 +0100

    update gitignore: add whole .idea folder delete several IDE files from repo
    remove redundant inheritance

--- src/main/java/nameserver/Nameserver.java
+++ src/main/java/nameserver/Nameserver.java
@@ -19,7 +19,7 @@ import util.Config;
  * Please note that this class is not needed for Lab 1, but will later be used
  * in Lab 2. Hence, you do not have to implement it for the first submission.
  */
-public class Nameserver implements INameserverCli, INameserver,INameserverForChatserver, Runnable {
+public class Nameserver implements INameserverCli, INameserver, Runnable {
 
 	private String componentName;
 	private Config config;
