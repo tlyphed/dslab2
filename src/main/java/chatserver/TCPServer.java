7c088f3cc38f3063ff0d342c88d3945258ce7d2c
mkamleithner <martin.kamleithner@gmail.com>
2017-01-06 16:43:43 +0100

    fix merge errors

--- src/main/java/chatserver/TCPServer.java
+++ src/main/java/chatserver/TCPServer.java
@@ -12,18 +12,19 @@ import java.net.Socket;
 
 public class TCPServer extends AbstractTCPServer {
 
-    private  final LookupRemoteHelper lookupRemoteHelper;
+    private final LookupRemoteHelper lookupRemoteHelper;
     private RegisterRemoteHelper registerHelper;
+    private EncryptedChannelServer channel;
 
-    public TCPServer(int port, INameserverForChatserver root){
+    public TCPServer(int port, INameserverForChatserver root) {
         super(port);
         this.lookupRemoteHelper = new LookupRemoteHelper(root);
         this.registerHelper = new RegisterRemoteHelper(root);
     }
 
     private void send(String message, TCPWorker sender) throws IOException {
-        for(TCPWorker worker : getWorkers()){
-            if(worker != sender && worker.getChannel() != null){
+        for (TCPWorker worker : getWorkers()) {
+            if (worker != sender && worker.getChannel() != null) {
                 worker.getChannel().write("!pubmsg: " + message);
             }
         }
@@ -36,96 +37,51 @@ public class TCPServer extends AbstractTCPServer {
 
     protected void processInput(TCPWorker worker, IChannel channel) throws IOException {
         String line;
-        User user = null;
-        while((line = channel.read()) != null && !Thread.currentThread().isInterrupted()){
+        User user = UserStore.getInstance().getUser(this.channel.getUsername());
+        if (user == null) {
+            throw new IOException("user unknown");
+        }
+        user.setOnline(true);
+        while ((line = channel.read()) != null && !Thread.currentThread().isInterrupted()) {
             String cmd[] = line.split(" ");
-            switch(cmd[0]){
-                case "login":
-                    if(cmd.length == 3){
-                        String name = cmd[1];
-                        String password = cmd[2];
-                        user = UserStore.getInstance().getUser(name);
-                        if(user != null){
-                            synchronized (user){
-                                if(!user.isOnline()){
-                                    if(user.getPassword().equals(password)){
-                                        user.setOnline(true);
-                                        channel.write("Successfully logged in.");
-                                        break;
-                                    }
-                                    channel.write("Wrong username or password.");
-                                    break;
-                                }
-                                channel.write("Already logged in.");
-                                break;
-                            }
-                        }
-                        channel.write("Unkown User.");
-                        break;
-                    }
-                    channel.write("Wrong argument.");
-                    break;
-                case "logout":
-                    if(cmd.length == 1){
-                        if(user != null){
-                            user.setOnline(false);
-                            user.setIpAddress(null);
-                            user = null;
-                            channel.write("Successfully logged out.");
-                            break;
-                        }
-                        channel.write("Not logged in.");
-                        break;
-                    }
-                    channel.write("Wrong argument.");
-                    break;
+            switch (cmd[0]) {
                 case "send":
-                    if(cmd.length > 1){
-                        if(user != null){
-                            String msg = line.substring(line.indexOf(" ") + 1);
-                            send(user.getName() + ": "+  msg, worker);
-                            channel.write("Message sent.");
-                            break;
-                        }
-                        channel.write("Not logged in.");
+                    if (cmd.length > 1) {
+
+                        String msg = line.substring(line.indexOf(" ") + 1);
+                        send(user.getName() + ": " + msg, worker);
+                        channel.write("Message sent.");
                         break;
+
                     }
                     channel.write("Wrong argument.");
                     break;
                 case "register":
-                    if(cmd.length == 2){
-                        if(user != null){
-                            String ipAddress = cmd[1];
-                            user.setIpAddress(ipAddress);
+                    if (cmd.length == 2) {
+                        String ipAddress = cmd[1];
+                        user.setIpAddress(ipAddress);
+                        channel.write("Successfully registered.");
+                        try {
+                            registerHelper.register(user);
                             channel.write("Successfully registered.");
-                            try {
-                                registerHelper.register(user);
-                                channel.write("Successfully registered.");
-                            }catch (InvalidDomainException e){
-                                channel.write("Invalid Domain!");
-                            }catch (AlreadyRegisteredException a){
-                                channel.write("Already registered!");
-                            }
-                            break;
+                        } catch (InvalidDomainException e) {
+                            channel.write("Invalid Domain!");
+                        } catch (AlreadyRegisteredException a) {
+                            channel.write("Already registered!");
                         }
-                        channel.write("Not logged in.");
                         break;
                     }
                     channel.write("Wrong argument.");
                     break;
                 case "lookup":
-                    if(cmd.length == 2){
-                        if(user != null){
-                            String lookupName = cmd[1];
-                            String lookupAddress = lookupRemoteHelper.lookupAddress(lookupName);
-                                if(lookupAddress != null){
-                                    channel.write(lookupAddress);
-                                    break;
-                                }
-                            channel.write("Wrong username or user not registered.");
+                    if (cmd.length == 2) {
+                        String lookupName = cmd[1];
+                        String lookupAddress = lookupRemoteHelper.lookupAddress(lookupName);
+                        if (lookupAddress != null) {
+                            channel.write(lookupAddress);
                             break;
                         }
-                        channel.write("Not logged in.");
+                        channel.write("Wrong username or user not registered.");
                         break;
                     }
                     channel.write("Wrong argument.");
@@ -137,7 +93,7 @@ public class TCPServer extends AbstractTCPServer {
                     channel.write("Wrong argument.");
             }
         }
-        if(user != null){
+        if (user != null) {
             user.setOnline(false);
             user.setIpAddress(null);
         }
